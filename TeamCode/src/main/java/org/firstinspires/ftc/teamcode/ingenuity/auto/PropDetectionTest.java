99fca3c419ce3348d2c515a741458efc42c79396
ERIKLAPTOP\eriki <erik@erik-anderson.net>
2024-02-23 21:22:28 -0500

    Prop detection testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ingenuity/auto/PropDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ingenuity/auto/PropDetectionTest.java
@@ -19,9 +19,9 @@ import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 @Config
 @Autonomous(name = "Prop Detection Test", group = "Auto 3.0 development")
 public final class PropDetectionTest extends LinearOpMode {
-    public static double initX = 12;
+    public static double initX = -36;
     public static double initY = 61;
-    public static double initAngle = 270;
+    public static double initAngle = -90;
     public static double pushX = initX;
     public static double pushY = initY - 35.5;
     public static double pushAngle = initAngle;
@@ -33,57 +33,45 @@ public final class PropDetectionTest extends LinearOpMode {
     public static double parkingY = 56;
     public static int backDelivery = 2250;
 
+    int propPosition ;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
-            PropDetection propDetector = new PropDetection(this);
+        PropDetection propDetector = new PropDetection(this);
 
 
-            PhaseTwoBot bot = new PhaseTwoBot(hardwareMap, telemetry);
-            TranslationalVelConstraint slow = new TranslationalVelConstraint(15);
+        PhaseTwoBot bot = new PhaseTwoBot(hardwareMap, telemetry);
+        TranslationalVelConstraint slow = new TranslationalVelConstraint(15);
 
-            Actions.runBlocking(new SequentialAction(
-                    bot.AutonomousInitActions(),
-                    bot.gripperArm().moveArmToPositionAction(PhaseTwoBot.armDropOne)
-            ));
-
-            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(initX, initY, Math.toRadians(initAngle)));
+        telemetry.addLine("Check 1") ;
+        updateTelemetry(telemetry);
 
 
-            sleep(5000);
-            int propPosition = propDetector.propTfod();
-            updateTelemetry(telemetry);
-            waitForStart();
+        Actions.runBlocking(new SequentialAction(
+                bot.AutonomousInitActions(),
+                bot.gripperArm().moveArmToPositionAction(PhaseTwoBot.armDropOne)
+        ));
 
-            Actions.runBlocking(drive.actionBuilder(drive.pose)
-                    .afterTime(0.0, bot.gripperArm().moveArmToStopAction(1, true))
-                    .splineTo(new Vector2d(pushX, pushY), Math.toRadians(pushAngle))
-                    .setReversed(true)
-                    .afterTime(0.0, bot.gripperArm().moveArmToPositionAction(backDelivery, "start moving", true))
-                    .splineTo(new Vector2d(11, 33), Math.toRadians(invPushAngle))
-                    .splineTo(new Vector2d(26, 42), Math.toRadians(0))
-                    .splineTo(new Vector2d(preDeliveryX, deliveryY), Math.toRadians(0))
-                    .splineTo(new Vector2d(deliveryX, deliveryY), Math.toRadians(0), slow)
-                    .stopAndAdd(new SequentialAction(
-                            new TimeoutAction(bot.gripperArm().moveArmToPositionAction(backDelivery, "finish moving", true), 2.5),
-                            bot.gripperArm().gripperOpenAction(),
-                            new SleepAction(0.5),
-                            bot.gripperArm().setWristTuckedUp(),
-                            bot.gripperArm().moveArmToStopAction(1, false)))
-                    .setReversed(false)
-                    .afterTime(0.0, new SequentialAction(
-                            bot.gripperArm().moveArmToStopAction(0),
-                            bot.gripperArm().lowerArmToLimit()
-                    ))
-                    .splineTo(new Vector2d(preDeliveryX, deliveryY), Math.toRadians(180))
-                    .strafeTo(new Vector2d(preDeliveryX, parkingY))
-                    .setReversed(true)
-                    .splineTo(new Vector2d(parkingX, parkingY), Math.toRadians(0))
-                    .build());
+        telemetry.addLine("Check 2") ;
+        updateTelemetry(telemetry);
+
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(initX, initY, Math.toRadians(initAngle)));
+
+
+        sleep(5000);
+        propPosition = propDetector.propTfod();
+        updateTelemetry(telemetry);
+        waitForStart();
+
+        Actions.runBlocking(drive.actionBuilder(drive.pose)
+                .splineTo(new Vector2d(initX, 54), Math.toRadians(-90))
+                .build());
+        sleep(5000);
+        propPosition = propDetector.propTfod();
+        updateTelemetry(telemetry);
 
 //            PoseStorage.currentPose = drive.pose;
-        } else {
-            throw new AssertionError();
-        }
+        while (opModeIsActive()) {}
     }
 }
