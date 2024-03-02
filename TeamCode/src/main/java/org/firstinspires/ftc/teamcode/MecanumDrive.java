764af52fcd025d860bf5482bc2095af9182cf570
ERIKLAPTOP\eriki <erik@erik-anderson.net>
2024-03-01 22:51:35 -0500

    Reduced wheel acceleration in MechanumDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -71,9 +71,9 @@ public final class MecanumDrive {
         public double kA = 0.0001;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 25;
+        public double maxWheelVel = 25;  // Default = 50
         public double minProfileAccel = -20;
-        public double maxProfileAccel = 30;
+        public double maxProfileAccel = 25;  // Default = 30
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
